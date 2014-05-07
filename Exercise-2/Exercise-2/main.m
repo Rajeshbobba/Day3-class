9fbabd436e1956547af21278f37628ea8d966ac2
Rajesh <rajeshbobba@yahoo.com>
2014-05-07 16:38:03 -0400

    Exercises

---
+++ Exercise-2/Exercise-2/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  Exercise-2
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 Rajesh Bobba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface Rectangle: NSObject
+-(void) setWidth: (int) W;
+-(void) setHeight: (int) h;
+-(int) width;
+-(int) height;
+-(int) area;
+-(int) parimeter;
+@end
+
+@implementation Rectangle
+{
+    
+}
+
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        
+    }
+    return 0;
+}
+
---
+++ Exercises-1/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  Exercises-1
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 Rajesh Bobba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int C;
+        int F=27;
+        
+        C=(F-32)/1.8;
+        NSLog (@"C= (F - 32)/1.8=%i",C);
+        
+        
+    }
+    return 0;
+}
+
